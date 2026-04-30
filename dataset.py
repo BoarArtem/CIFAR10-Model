eb5e1c6ea2e00e8ae05d16b62b81341d61bf4a0a
BoarArtem <workboar@gmail.com>
2026-04-30 17:10:17 +0300

    make some changing

--- dataset.py
+++ dataset.py
@@ -1,13 +1,23 @@
 import torch
-from torch.utils.data import DataLoader
+import torch.nn as nn
 import torchvision.transforms as transforms
-import torchvision.datasets as datasets
+from torchvision.datasets  import CIFAR10
+from torch.utils.data import DataLoader
 
-transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465,), (0.247, 0.243, 0.261,))])
 
-train_dataset = datasets.CIFAR10(root='./data', train=True, transform=transforms, download=True)
-test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transforms)
+train_dataset = CIFAR10(root="/data", train=True, transform=transforms, download=True)
+test_dataset = CIFAR10(root="/data", train=False)
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+def get_train_loader(batch_size = 32, shuffle = True):
+    return DataLoader(train_dataset, batch_size, shuffle)
 
+def get_test_loader(batch_size = 32, shuffle = False):
+    return DataLoader(test_dataset, batch_size, shuffle)
+
+
+for X_batch, y_batch in get_train_loader():
+    print(X_batch.shape)
+    print(y_batch.shape)
+
+    break
--- model.py
+++ model.py
@@ -2,32 +2,44 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from dataset import train_loader, test_loader
+from dataset import get_train_loader
 
 
 class CIFAR10Model(nn.Module):
     def __init__(self):
         super(CIFAR10Model, self).__init__()
+        # (batch_size, channel, height, width)
+        # (32, 3, 32, 32)
 
         self.conv_layer = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1),
+            # feature - (32 - 3 + 2)/1 + 1 = 32
+            # (32, 32, 32)
             nn.BatchNorm2d(32),
-            nn.MaxPool2d(2),
             nn.ReLU(),
+            nn.MaxPool2d(2),
+            # 32 / 2 = 16
+            # (32, 16, 16)
 
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3,stride=1, padding=1),
             nn.BatchNorm2d(64),
-            nn.MaxPool2d(2),
             nn.ReLU(),
+            nn.MaxPool2d(2),
+            # 16 / 2 = 8
+            # (64, 8, 8)
         )
 
         self.fc_layer = nn.Sequential(
+            # Linear - (batch_size, features)
+            # linear input - 64 * 8 * 8 = 4096
             nn.Flatten(),
 
-            nn.Linear(64 * 8 * 8, 128),
+            nn.Linear(4096, 128),
             nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(128, 10)
+            nn.Dropout(0.4),
+
+            nn.Linear(128, 10),
         )
 
     def forward(self, x):
@@ -37,16 +49,23 @@ class CIFAR10Model(nn.Module):
         return x
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = CIFAR10Model().to(device)
 
-criterion = nn.CrossEntropyLoss()
-optim = optim.Adam(model.parameters(), lr=0.001)
+def get_cifar_model():
+    return CIFAR10Model().to(device)
 
-num_epoch = 10
+def get_optimization(model):
+    return optim.Adam(model.parameters(), lr=0.001)
 
-if __name__ == "__main__":
-    for epoch in range(num_epoch):
+def get_criterion():
+    return nn.CrossEntropyLoss()
+
+
+def train(model, optim, criterion, train_loader, num_epochs = 10):
+    print("Starting training...")
+    for epoch in range(num_epochs):
         training_loss = 0
+        correct = 0
+        total = 0
 
         model.train()
 
@@ -59,29 +78,22 @@ if __name__ == "__main__":
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
+
             optim.step()
 
             training_loss += loss.item()
 
-        print(f"Epoch: {epoch + 1}/{num_epoch}, Loss: {training_loss}")
-        torch.save(model.state_dict(), "cifar_model.pth")
-
-    correct = 0
-    total = 0
-
-    with torch.no_grad():
-        model.eval()
-
-        for inputs, labels in test_loader:
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            outputs = model(inputs)
-
-            _, predicted = torch.max(outputs.data, 1)
+            preds = torch.argmax(outputs, dim=1)
+            correct += (preds == labels).sum().item()
             total += labels.size(0)
 
-            correct += (predicted == labels).sum().item()
+        print(f"Epoch: {epoch+1}/{num_epochs}, Loss: {(training_loss / len(train_loader)):.4f}, Accuracy: {(correct/total)*100:.4f}")
+        torch.save(model.state_dict(), "cifar10_model.pth")
 
-        print(f"Accuracy: {100 * (correct / total)}%")
-        # after training, I got 74% accuracy
+if __name__ == "__main__":
+    model = get_cifar_model()
+    optim = get_optimization(model)
+    criterion = get_criterion()
+    train_loader = get_train_loader()
+
+    train(model, optim, criterion, train_loader)
