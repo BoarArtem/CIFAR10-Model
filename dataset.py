c4a41e649251c34cf35c0f7716b222ca0769070a
BoarArtem <workboar@gmail.com>
2026-05-04 01:18:35 +0300

    Switch dataset from CIFAR10 to FashionMNIST

--- dataset.py
+++ dataset.py
@@ -1,23 +1,22 @@
-import torch
-import torch.nn as nn
 import torchvision.transforms as transforms
-from torchvision.datasets  import CIFAR10
+from torchvision.datasets import FashionMNIST
 from torch.utils.data import DataLoader
 
-transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465,), (0.247, 0.243, 0.261,))])
+transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 
-train_dataset = CIFAR10(root="/data", train=True, transform=transforms, download=True)
-test_dataset = CIFAR10(root="/data", train=False)
+train_dataset = FashionMNIST("/data", train=True, download=True, transform=transforms)
+test_dataset = FashionMNIST("/data", train=False, download=False)
 
 def get_train_loader(batch_size = 32, shuffle = True):
     return DataLoader(train_dataset, batch_size, shuffle)
 
 def get_test_loader(batch_size = 32, shuffle = False):
-    return DataLoader(test_dataset, batch_size, shuffle)
+    return DataLoader(train_dataset, batch_size, shuffle)
 
+train_loader = get_train_loader()
 
-for X_batch, y_batch in get_train_loader():
-    print(X_batch.shape)
-    print(y_batch.shape)
+for inputs, labels in train_loader:
+    print(inputs.shape)
+    print(labels.shape)
 
     break
